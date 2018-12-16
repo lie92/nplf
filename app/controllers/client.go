5e3864638d29d3c5180c4895d707a169930ed5ef
admin <bastien.letelliet@epita.fr>
2018-12-16 16:32:43 +0100

    [CLIENT] Facture controller comunicate with the bdd

--- app/controllers/client.go
+++ app/controllers/client.go
@@ -1,11 +1,10 @@
 package controllers
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/revel/revel"
-	"nlpf/app/models"
 	"nlpf/app"
+	"nlpf/app/models"
 	"time"
 )
 
@@ -20,39 +19,37 @@ type Client struct {
 }
 
 func (c Client) Index() revel.Result {
-	tag1 := models.Tag{UID: 1, UserId: 1, Time: "02/10/18", Place: "lol", Accepted: true, Reason: "Non"}
-	tag2 := models.Tag{UID: 2, UserId: 1, Time: "03/10/18", Place: "lol", Accepted: true, Reason: "Non"}
-	tag3 := models.Tag{UID: 3, UserId: 1, Time: "04/10/18", Place: "lol", Accepted: true, Reason: "Non"}
 
-	tags := [3]models.Tag{tag1, tag2, tag3}
-	price := 0
-	for e := range tags {
-		price += tags[e].Price
-	}
-	return c.Render(tags, price)
+	return c.Render()
 }
 
 func (c Client) Facture() revel.Result {
-	sqlStatement := `SELECT * FROM tags WHERE id=$1`
+	sqlStatement := `SELECT * FROM tags WHERE userId=$1`
 	rows, err := app.Db.Query(sqlStatement, 1)
 	checkErr(err)
+	var total = 0
+
+	
+	var tags []models.Tag
 	for rows.Next() {
+
 		var uid int
-		var userId string
+		var userId int
 		var time time.Time
 		var place string
-		err = rows.Scan(&uid, &username, &department, &created)
+		var accepter bool
+		var reason string
+		var price int
+
+		total += price
+		err = rows.Scan(&uid, &userId, &time, &place, &accepter, &reason, &price)
 		checkErr(err)
-		fmt.Println("uid | username | department | created ")
-		fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+		fmt.Println("test")
+		tags = append(tags, models.Tag{uid, userId, time, place, accepter,
+			reason, price})
 	}
 
-	tags := [3]models.Tag{tag1, tag2, tag3}
-	price := 0
-	for e := range tags {
-		price += tags[e].Price
-	}
-	return c.Render(tags, price)
+	return c.Render(tags, total)
 }
 
 func (c Client) Demande() revel.Result {
